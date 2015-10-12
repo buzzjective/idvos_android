bb2c356e65cf7a70429aaedcb6569d3369468f6c
Zaitsewa Anastasia <biovamp@Zaitsewas-MacBook-Air.local>
2015-10-12 12:11:20 +0200

    Mode: production

--- example/src/main/java/de/idvos/example/ExampleApplication.java
+++ example/src/main/java/de/idvos/example/ExampleApplication.java
@@ -8,6 +8,6 @@ public class ExampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        IdvosSDK.initialize(this, IdvosSDK.Mode.TEST);
+        IdvosSDK.initialize(this, IdvosSDK.Mode.PRODUCTION);
     }
 }
